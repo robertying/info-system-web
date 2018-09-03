d47ecddfb001d803418bb093bc67e2ab28892d39
Robert Ying <yingrui205@gmail.com>
2018-09-02 23:05:40 -0600

    Revert to date-fns alpha 7

--- src/components/eventDialog.js
+++ src/components/eventDialog.js
@@ -94,9 +94,9 @@ class EventDialog extends React.Component {
       steps: this.state.stepContents.map((n, index) => {
         return `${n}\n起 ${format(
           this.state.selectedBeginDate[index],
-          "yyyy-MM-dd HH:mm",
+          "YYYY-MM-DD HH:mm",
           { locale: CNLocale }
-        )}\n止 ${format(this.state.selectedEndDate[index], "yyyy-MM-dd HH:mm", {
+        )}\n止 ${format(this.state.selectedEndDate[index], "YYYY-MM-DD HH:mm", {
           locale: CNLocale
         })}`;
       }),
