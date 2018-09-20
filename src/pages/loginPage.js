b6c0d462896adfcaf37710efc589216ad48da387
Robert Ying <yingrui205@gmail.com>
2018-09-20 11:57:37 -0600

    Responsive page for mobile devices

--- src/pages/loginPage.js
+++ src/pages/loginPage.js
@@ -13,6 +13,7 @@ import Input from "@material-ui/core/Input";
 import InputLabel from "@material-ui/core/InputLabel";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import IconButton from "@material-ui/core/IconButton";
+import Hidden from "@material-ui/core/Hidden";
 import Visibility from "@material-ui/icons/Visibility";
 import VisibilityOff from "@material-ui/icons/VisibilityOff";
 import Card from "@material-ui/core/Card";
@@ -34,15 +35,23 @@ const styles = theme => ({
   },
   card: {
     maxWidth: 300,
-    marginTop: "2.5%",
-    height: "70%",
-    paddingTop: "5%",
-    paddingBottom: "5%",
+    width: "100vw",
+    maxHeight: 500,
+    height: "100vh",
+    textAlign: "center",
+    marginTop: "3vh"
+  },
+  cardMobile: {
+    width: "100vw",
+    height: `calc(100vh - 56px)`,
     textAlign: "center"
   },
   logoImage: {
-    width: "40%",
-    margin: theme.spacing.unit
+    width: "25vh",
+    maxWidth: "50%",
+    margin: theme.spacing.unit,
+    marginTop: "18%",
+    marginBottom: "10%"
   },
   textField: {
     width: "65%",
@@ -50,7 +59,7 @@ const styles = theme => ({
   },
   button: {
     margin: theme.spacing.unit,
-    marginTop: "15%"
+    marginTop: "13%"
   }
 });
 
@@ -109,56 +118,110 @@ class LoginPage extends React.Component {
           </Toolbar>
         </AppBar>
         <div className={classes.flex}>
-          <Card className={classes.card} raised>
-            <img className={classes.logoImage} src={Logo} alt="logo" />
-            <div>
-              <TextField
-                id="username"
-                label="用户名"
-                placeholder="学号 / 邮箱"
-                className={classes.textField}
-                value={this.state.id}
-                onChange={this.handleChange("id")}
-                margin="normal"
-                spellCheck="false"
-              />
-            </div>
-            <div>
-              <FormControl className={classes.textField}>
-                <InputLabel htmlFor="adornment-password">密码</InputLabel>
-                <Input
+          <Hidden mdUp>
+            <Card className={classes.cardMobile} elevation={0} square>
+              <img className={classes.logoImage} src={Logo} alt="logo" />
+              <div>
+                <TextField
+                  id="username"
+                  label="用户名"
+                  placeholder="学号 / 邮箱"
+                  className={classes.textField}
+                  value={this.state.id}
+                  onChange={this.handleChange("id")}
+                  margin="normal"
                   spellCheck="false"
-                  id="adornment-password"
-                  type={this.state.showPassword ? "text" : "password"}
-                  value={this.state.password}
-                  onChange={this.handleChange("password")}
-                  endAdornment={
-                    <InputAdornment position="end">
-                      <IconButton
-                        aria-label="Toggle password visibility"
-                        onClick={this.handleClickShowPassword}
-                        onMouseDown={this.handleMouseDownPassword}
-                      >
-                        {this.state.showPassword ? (
-                          <VisibilityOff />
-                        ) : (
-                          <Visibility />
-                        )}
-                      </IconButton>
-                    </InputAdornment>
-                  }
                 />
-              </FormControl>
-            </div>
-            <Button
-              className={classes.button}
-              variant="raised"
-              color="primary"
-              onClick={this.handleFormSubmit}
-            >
-              登录
-            </Button>
-          </Card>
+              </div>
+              <div>
+                <FormControl className={classes.textField}>
+                  <InputLabel htmlFor="adornment-password">密码</InputLabel>
+                  <Input
+                    spellCheck="false"
+                    id="adornment-password"
+                    type={this.state.showPassword ? "text" : "password"}
+                    value={this.state.password}
+                    onChange={this.handleChange("password")}
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="Toggle password visibility"
+                          onClick={this.handleClickShowPassword}
+                          onMouseDown={this.handleMouseDownPassword}
+                        >
+                          {this.state.showPassword ? (
+                            <VisibilityOff />
+                          ) : (
+                            <Visibility />
+                          )}
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                  />
+                </FormControl>
+              </div>
+              <Button
+                className={classes.button}
+                variant="raised"
+                color="primary"
+                onClick={this.handleFormSubmit}
+              >
+                登录
+              </Button>
+            </Card>
+          </Hidden>
+          <Hidden smDown implementation="css">
+            <Card className={classes.card} raised>
+              <img className={classes.logoImage} src={Logo} alt="logo" />
+              <div>
+                <TextField
+                  id="username"
+                  label="用户名"
+                  placeholder="学号 / 邮箱"
+                  className={classes.textField}
+                  value={this.state.id}
+                  onChange={this.handleChange("id")}
+                  margin="normal"
+                  spellCheck="false"
+                />
+              </div>
+              <div>
+                <FormControl className={classes.textField}>
+                  <InputLabel htmlFor="adornment-password">密码</InputLabel>
+                  <Input
+                    spellCheck="false"
+                    id="adornment-password"
+                    type={this.state.showPassword ? "text" : "password"}
+                    value={this.state.password}
+                    onChange={this.handleChange("password")}
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="Toggle password visibility"
+                          onClick={this.handleClickShowPassword}
+                          onMouseDown={this.handleMouseDownPassword}
+                        >
+                          {this.state.showPassword ? (
+                            <VisibilityOff />
+                          ) : (
+                            <Visibility />
+                          )}
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                  />
+                </FormControl>
+              </div>
+              <Button
+                className={classes.button}
+                variant="raised"
+                color="primary"
+                onClick={this.handleFormSubmit}
+              >
+                登录
+              </Button>
+            </Card>
+          </Hidden>
         </div>
       </div>
     );
